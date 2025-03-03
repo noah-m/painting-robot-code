c7cfd049f85c115c4e7469045faffd2ab87af0b9
Noah Mould <43688099+noah-m@users.noreply.github.com>
2025-03-02 16:50:13 -0800

    i pasted linear actuator code

--- Arduino/src/main.cpp
+++ Arduino/src/main.cpp
@@ -1,23 +1,22 @@
-#include <Arduino.h>
 #include <AccelStepper.h>
 
-// put function declarations here:
-int myFunction(int, int);
+// Define pin connections
+#define STEP_PIN 3  // Connect to PUL-
+#define DIR_PIN 2   // Connect to DIR-
+
+// Create an AccelStepper object in DRIVER mode (1 = driver mode)
+AccelStepper stepper(AccelStepper::DRIVER, STEP_PIN, DIR_PIN);
 
 void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+  // Set the maximum speed and acceleration
+  stepper.setMaxSpeed(3000);    // Steps per second
+  stepper.setAcceleration(200); // Steps per second^2
+
+  // Set the initial speed (positive for one direction, negative for the other)
+  stepper.setSpeed(-2000); // Steps per second
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  // Continuously run the motor at the set speed
+  stepper.runSpeed();
 }
-
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
-}
-
-// change change change
-// ahhhhhhh
-// i added this in github
