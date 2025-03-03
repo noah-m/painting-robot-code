dffca24618a7a0f69f7fdd28cd1142348a181488
Noah Mould <43688099+noah-m@users.noreply.github.com>
2025-03-03 13:25:39 -0800

    more files added

--- Arduino/src/main.cpp
+++ Arduino/src/main.cpp
@@ -1,27 +1,45 @@
 #include <AccelStepper.h>
+#include <EEPROM.h>
+
 #include <pins.h>
+#include <driving.h>
+#include <callibration.h>
+
+// Robot states
+enum ROBOT_STATES {CALLIBRATING, STATIONARY, PAINTING};
+ROBOT_STATES robot_state = CALLIBRATING;
+
+//Prototypes
+void callibrate();
 
 // Create an AccelStepper object in DRIVER mode (1 = driver mode)
 AccelStepper stepper(AccelStepper::DRIVER, STEP_PIN, DIR_PIN);
 
 void setup() {
   // Set the maximum speed and acceleration
-  stepper.setMaxSpeed(3000);    // Steps per second
-  stepper.setAcceleration(200); // Steps per second^2
+  stepper.setMaxSpeed(3000);  // Steps per second
+  stepper.setAcceleration(0); // Steps per second^2
 
   // Set the initial speed (positive for one direction, negative for the other)
-  stepper.setSpeed(-2000); // Steps per second
-
-  if (VERT_LIMIT == HIGH)
-  {
-    Serial.println("On");
-  }
-  
+  stepper.setSpeed(100); // Steps per second
 }
 
 void loop() {
-  // Continuously run the motor at the set speed
-  stepper.runSpeed();
+
+  switch (robot_state) {
+    case CALLIBRATING:
+      callibrate();
+      break;
+    case STATIONARY:
+      // stationary stuff
+      break;
+    case PAINTING:
+      // painting stuff
+      break;
+  }
 }
 
-// comment ia m love to see it
+void callibrate() {
+  
+
+}
