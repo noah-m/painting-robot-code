54b117061e55d74ba15be9450b582c8817b82aa6
Noah Mould <43688099+noah-m@users.noreply.github.com>
2025-03-03 17:22:48 -0800

    Two source files now

---
+++ Arduino/src/calibration.cpp
@@ -0,0 +1,83 @@
+#include "calibration.h"
+#include <AccelStepper.h>
+#include <EEPROM.h>
+#include "globals.h"
+
+void calibrate() {
+
+    // ## If zeroing button is pushsed on start-up, enter manual zeroing mode. Otherwise, use location from EEPROM.
+    if (ZERO_BUTTON == PUSHED) {
+      // Manual zeroing
+      stepper.setSpeed(-100);
+      while (ZERO_BUTTON == PUSHED)
+      {
+        stepper.runSpeed();
+      }
+      savePositionToEEPROM(0);
+      stepper.setCurrentPosition(0);
+    }
+    else {
+      // Retrieve position from EEPROM
+      stepper.setCurrentPosition(getPositionFromEEPROM());
+    }
+  
+    // Move carriadge to the top
+    stepper.moveTo(2000);
+    stepper.runToPosition();
+  
+    // Move limit switch assembly towards wall until first limit switch hit
+    int angle = MIN_SERVO_ANGLE;
+    while (true)
+    {
+      while (HORZ_LIMIT == NOT_PUSHED)
+      {
+      angle++;
+      analogWrite(SERVO, angle);
+      delay(100);
+      }
+      angle--;
+      analogWrite(SERVO, angle);
+      delay(500);
+      if (HORZ_LIMIT == PUSHED)
+      {
+        break;
+      }
+    }
+  
+    // TODO: Convert angle to wall distance
+  
+    // Move back down until other limit switch hit
+    stepper.setSpeed(-100);
+    while (NOT_PUSHED)
+    {
+      stepper.runSpeed();
+    }
+    
+  
+    
+    
+    
+  
+    
+  
+  }
+  
+  void savePositionToEEPROM(long position) {
+    // Automatically stores position over multiple memory cells starting from cell zero
+    EEPROM.put(EEPROM_POSITION_ADDR, position);
+    Serial.print("Position ");
+    Serial.print(position);
+    Serial.println(" saved to EEPROM");
+  
+  }
+  
+  long getPositionFromEEPROM(void) {
+    // Automatically stores position over multiple memory cells specificed by input of type long starting from cell zero
+    long position;
+    EEPROM.get(EEPROM_POSITION_ADDR, position);
+    Serial.print("Position ");
+    Serial.print(position);
+    Serial.println(" retrived from EEPROM");
+    return position;
+  }
+
--- Arduino/src/main.cpp
+++ Arduino/src/main.cpp
@@ -1,14 +1,9 @@
 #include <AccelStepper.h>
 #include <EEPROM.h>
 
-#include <pins.h>
+#include <define.h>
 #include <driving.h>
-#include <callibration.h>
-
-#define EEPROM_POSITION_ADDR 0
-#define MAX_STEPPER_POSITION 2000
-#define MIN_SERVO_ANGLE 50
-#define MAX_SERVO_ANGLE 205
+#include <calibration.h>
 
 //Prototypes
 void callibrate();
@@ -19,7 +14,7 @@ long getPositionFromEEPROM(void);
 AccelStepper stepper(AccelStepper::DRIVER, STEP_PIN, DIR_PIN);
 
 // Robot states
-enum ROBOT_STATES {CALLIBRATING, STATIONARY, PAINTING};
+enum ROBOT_STATES {CALIBRATING, STATIONARY, PAINTING};
 ROBOT_STATES robot_state;
 
 void setup() {
@@ -33,13 +28,13 @@ void setup() {
   stepper.setSpeed(100); // Steps per second
 
   // Set initial state
-  robot_state = CALLIBRATING;
+  robot_state = CALIBRATING;
 }
 
 void loop() {
 
   switch (robot_state) {
-    case CALLIBRATING:
+    case CALIBRATING:
       callibrate();
       break;
     case STATIONARY:
@@ -51,74 +46,3 @@ void loop() {
   }
 }
 
-void callibrate() {
-
-  // ## If zeroing button is pushsed on start-up, enter manual zeroing mode. Otherwise, use location from EEPROM.
-  if (ZERO_BUTTON == LOW) {
-    // Manual zeroing
-    stepper.setSpeed(-100);
-    while (ZERO_BUTTON == LOW)
-    {
-      stepper.runSpeed();
-    }
-    savePositionToEEPROM(0);
-    stepper.setCurrentPosition(0);
-  }
-  else {
-    // Retrieve position from EEPROM
-    stepper.setCurrentPosition(getPositionFromEEPROM());
-  }
-
-  // Move carriadge to the top
-  stepper.moveTo(2000);
-  stepper.runToPosition();
-
-  // Move limit switch assembly towards wall until first limit switch hit
-  int angle = MIN_SERVO_ANGLE;
-  while (true)
-  {
-    while (HORZ_LIMIT == HIGH)
-    {
-    angle++;
-    analogWrite(SERVO, angle);
-    delay(100);
-    }
-    angle--;
-    analogWrite(SERVO, angle);
-    delay(500);
-    if (HORZ_LIMIT == LOW)
-    {
-      break;
-    }
-  }
-
-  // TODO: Convert angle to wall distance
-
-  // Move back down until other limit switch hit
-
-  
-  
-  
-
-  
-
-}
-
-void savePositionToEEPROM(long position) {
-  // Automatically stores position over multiple memory cells starting from cell zero
-  EEPROM.put(EEPROM_POSITION_ADDR, position);
-  Serial.print("Position ");
-  Serial.print(position);
-  Serial.println(" saved to EEPROM");
-
-}
-
-long getPositionFromEEPROM(void) {
-  // Automatically stores position over multiple memory cells specificed by input of type long starting from cell zero
-  long position;
-  EEPROM.get(EEPROM_POSITION_ADDR, position);
-  Serial.print("Position ");
-  Serial.print(position);
-  Serial.println(" retrived from EEPROM");
-  return position;
-}
