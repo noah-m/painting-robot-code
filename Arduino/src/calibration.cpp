bd02ccecb268d58d7bcdf2c387789707ccea58df
Noah Mould <43688099+noah-m@users.noreply.github.com>
2025-03-04 16:05:37 -0800

    finished calibration code maybe

--- Arduino/src/calibration.cpp
+++ Arduino/src/calibration.cpp
@@ -4,80 +4,93 @@
 #include "globals.h"
 
 void calibrate() {
-
-    // If zeroing button is pushsed on start-up, enter manual zeroing mode. Otherwise, use location from EEPROM.
     if (ZERO_BUTTON == PUSHED) {
-      // Manual zeroing
-      stepper.setSpeed(-DEFAULT_SPEED);
-      while (ZERO_BUTTON == PUSHED)
-      {
+        manualZeroing();
+    } else {
+        retrievePositionFromEEPROM();
+    }
+
+    moveToTop();
+    long wallDistance = findWallDistance();
+    long trimDistnace = findTrimDistance();
+    savePositionToEEPROM(stepper.currentPosition());
+    resetServo();
+
+    Serial.println("Calibration complete");
+    Serial.print("Wall distance: ");
+    Serial.println(wallDistance);
+    Serial.print("Trim distance: ");
+    Serial.println(trimDistnace);
+}
+
+void manualZeroing() {
+    stepper.setSpeed(-DEFAULT_SPEED);
+    while (ZERO_BUTTON == PUSHED) {
         stepper.runSpeed();
-      }
-      savePositionToEEPROM(0);
-      stepper.setCurrentPosition(0);
     }
-    else {
-      // Retrieve position from EEPROM
-      stepper.setCurrentPosition(getPositionFromEEPROM());
-    }
-  
-    // Move carriadge to the top
-    stepper.moveTo(2000);
+    savePositionToEEPROM(0);
+    stepper.setCurrentPosition(0);
+}
+
+void retrievePositionFromEEPROM() {
+    stepper.setCurrentPosition(getPositionFromEEPROM());
+}
+
+void moveToTop() {
+    stepper.moveTo(TOP_POSITION);
     stepper.runToPosition();
-  
-    // Move limit switch assembly towards wall until first limit switch hit
+    savePositionToEEPROM(TOP_POSITION);
+}
+
+long findWallDistance() {
     int angle = MIN_SERVO_ANGLE;
-    while (true)
-    {
-      while (HORZ_LIMIT == NOT_PUSHED)
-      {
-      angle++;
-      analogWrite(SERVO, angle);
-      delay(100);
-      }
-      angle--;
-      analogWrite(SERVO, angle);
-      delay(500);
-      if (HORZ_LIMIT == PUSHED)
-      {
-        break;
-      }
+    analogWrite(SERVO, angle);
+    delay(SERVO_DELAY);
+
+    while (true) {
+        if (HORZ_LIMIT == NOT_PUSHED) {
+            angle++;
+            analogWrite(SERVO, angle);
+            delay(SERVO_DELAY);
+        } else {
+            angle--;
+            analogWrite(SERVO, angle);
+            delay(SERVO_BACK_DELAY);
+            if (HORZ_LIMIT == PUSHED) {
+                break;
+            }
+        }
     }
-  
+
     // TODO: Convert angle to wall distance
-  
-    // Move back down until other limit switch hit
-    stepper.setSpeed(-100);
-    while (NOT_PUSHED)
-    {
-      stepper.runSpeed();
+}
+
+long findTrimDistance() {
+    stepper.setSpeed(DEFAULT_SPEED);
+    while (NOT_PUSHED) {
+        stepper.runSpeed();
     }
-    
-  
-    
-    
-    
-  
-    
-  
-  }
-  
-  void savePositionToEEPROM(long position) {
-    // Automatically stores position over multiple memory cells starting from cell zero
+    return stepper.currentPosition();
+}
+
+void resetServo() {
+    analogWrite(SERVO, MIN_SERVO_ANGLE);
+    delay(SERVO_DELAY);
+}
+
+void savePositionToEEPROM(long position) {
     EEPROM.put(EEPROM_POSITION_ADDR, position);
     Serial.print("Position ");
     Serial.print(position);
     Serial.println(" saved to EEPROM");
-  
-  }
-  
-  long getPositionFromEEPROM(void) {
-    // Automatically stores position over multiple memory cells specificed by input of type long starting from cell zero
+}
+
+long getPositionFromEEPROM() {
     long position;
     EEPROM.get(EEPROM_POSITION_ADDR, position);
     Serial.print("Position ");
     Serial.print(position);
-    Serial.println(" retrived from EEPROM");
+    Serial.println(" retrieved from EEPROM");
     return position;
-  }
+}
 
